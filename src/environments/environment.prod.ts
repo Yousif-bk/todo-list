43c0fa78bccf9e8ca070429791a099408310bd15
Yousif Babiker <yousif.bk@datacellme.com>
2022-12-12 09:34:37 +0000

    [fix] fixed deploy issues

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyC6Hwsf40w6RnHKpfRGYxAxu5zAodQ8tZg",
+    authDomain: "todo-list-ff57b.firebaseapp.com",
+    projectId: "todo-list-ff57b",
+    storageBucket: "todo-list-ff57b.appspot.com",
+    messagingSenderId: "419997570802",
+    appId: "1:419997570802:web:71c07143fc5dc32a96626b",
+    measurementId: "G-HMJX0E4WNV"
+  }
 };
